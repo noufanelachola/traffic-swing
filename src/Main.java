2c9805fa359ef22dd8ebc4f4949b250285c36ea3
noufanelachola <noufansadiqelachola@gmail.com>
2024-09-21 22:02:13 +0530

    panels added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,9 +7,21 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Traffic");
         frame.setSize(800, 600);
+        frame.setLayout(new FlowLayout());
 
         ImageIcon icon = new ImageIcon("D:\\Codes\\Java\\Swing\\Projects\\04-Traffic\\traffic-lights.png");
         frame.setIconImage(icon.getImage());
+
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(400, 600));
+        panel.setBackground(Color.BLACK);
+
+        JPanel panel2 = new JPanel();
+        panel2.setPreferredSize(new Dimension(400, 600));
+        panel2.setBackground(Color.GREEN);
+
+        frame.add(panel);
+        frame.add(panel2);
 
         frame.setVisible(true);
     }
