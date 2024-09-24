ec1f2354717e9d075bc51886ab9a7e3651b0a70a
noufanelachola <noufansadiqelachola@gmail.com>
2024-09-24 07:55:42 +0530

    Radio buttons added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,26 +15,39 @@ public class Main {
         frame.setIconImage(icon.getImage());
 
         JPanel panel = new JPanel() {
+
+            Color redColor = Color.DARK_GRAY;
+            Color yellowColor = Color.DARK_GRAY;
+            Color greenColor = Color.DARK_GRAY;
+
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
 
                 // Set color for the outer rectangle (traffic light box)
                 g.setColor(Color.DARK_GRAY);
-                g.fillRect(100, 50, 200, 400); // x, y, width, height
+                g.fillRect(100, 50, 200, 400);
 
-                // Set color and draw the red light
-                g.setColor(Color.RED);
-                g.fillOval(150, 75, 100, 100); // x, y, width, height
+                // Draw red light
+                g.setColor(redColor);
+                g.fillOval(150, 75, 100, 100);
 
-                // Set color and draw the yellow light
-                g.setColor(Color.YELLOW);
-                g.fillOval(150, 200, 100, 100); // x, y, width, height
+                // Draw yellow light
+                g.setColor(yellowColor);
+                g.fillOval(150, 200, 100, 100);
 
-                // Set color and draw the green light
-                g.setColor(Color.GREEN);
-                g.fillOval(150, 325, 100, 100); // x, y, width, height
+                // Draw green light
+                g.setColor(greenColor);
+                g.fillOval(150, 325, 100, 100);
             }
+
+            public void changeLight(String light) {
+                redColor = light.equals("Red") ? Color.RED : Color.DARK_GRAY;
+                yellowColor = light.equals("Yellow") ? Color.YELLOW : Color.DARK_GRAY;
+                greenColor = light.equals("Green") ? Color.GREEN : Color.DARK_GRAY;
+                repaint();  // Repaint the panel with updated colors
+            }
+
         };
         panel.setPreferredSize(new Dimension(400, 600));
         panel.setBackground(Color.BLACK);
@@ -41,10 +56,50 @@ public class Main {
 
         JPanel panel2 = new JPanel();
         panel2.setPreferredSize(new Dimension(400, 600));
+        panel2.setLayout(new FlowLayout(FlowLayout.CENTER));
         panel2.setBackground(Color.GREEN);
+
+        JRadioButton redBtn = new JRadioButton("Red");
+        JRadioButton yellowBtn = new JRadioButton("Yellow");
+        JRadioButton greenBtn = new JRadioButton("Green");
+
+        ButtonGroup btnGroup = new ButtonGroup();
+        btnGroup.add(redBtn);
+        btnGroup.add(yellowBtn);
+        btnGroup.add(greenBtn);
+
+        redBtn.setOpaque(false);
+        yellowBtn.setOpaque(false);
+        greenBtn.setOpaque(false);
+
+        redBtn.setFocusable(false);
+        yellowBtn.setFocusable(false);
+        greenBtn.setFocusable(false);
+
+        panel2.add(redBtn);
+        panel2.add(yellowBtn);
+        panel2.add(greenBtn);
+
+        ActionListener actionListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == redBtn) {
+                    panel.changeLight("Red");
+                } else if (e.getSource() == yellowBtn) {
+                    panel.changeLight("Yellow");
+                } else if (e.getSource() == greenBtn) {
+                    panel.changeLight("Green");
+                }
+            }
+        };
+
+        redBtn.addActionListener(actionListener);
+        yellowBtn.addActionListener(actionListener);
+        greenBtn.addActionListener(actionListener);
 
         frame.add(panel);
         frame.add(panel2);
+
 
         frame.setVisible(true);
     }
